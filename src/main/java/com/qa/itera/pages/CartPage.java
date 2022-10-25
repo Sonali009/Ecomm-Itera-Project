d4895037e1bb1c794fd8d718e7ea03ed92e608e1
Sonali009 <sonalispadhi09@gmail.com>
2022-10-25 20:58:36 +0530

    updated the PR Comments

--- src/main/java/com/qa/itera/pages/CartPage.java
+++ src/main/java/com/qa/itera/pages/CartPage.java
@@ -1,9 +1,16 @@
 package com.qa.itera.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
+	private By cartButton= By.id("cart");
+	
 	public CartPage() {
 		System.out.println("my cart page.......");
 	}
-	
+		
+	public void clickOnCart() {
+		System.out.println(cartButton);
+	}
 }
